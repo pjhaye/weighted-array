692aebdfaeb6460942f19d2a502cea51eb142e6b
PJ Haye <pj.haye@gmail.com>
2022-09-11 13:51:43 -0700

    Initial commit

---
+++ Scripts/WeightedArray.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+namespace WeightedArrays
+{
+    public class WeightedArray<T> 
+    {    
+        private List<WeightedArrayEntry<T>> _weightedArrayEntries;
+
+        public List<WeightedArrayEntry<T>> WeightedArrayEntries => _weightedArrayEntries;
+        
+        public int NumChoices
+        {
+            get
+            {
+                return _weightedArrayEntries.Count;
+            }
+        }
+
+        public WeightedArray()
+        {
+            _weightedArrayEntries = new List<WeightedArrayEntry<T>>();
+        }
+
+        public void AddItem(T item, float weight)
+        {
+            _weightedArrayEntries.Add(new WeightedArrayEntry<T>(item, weight));
+        }
+
+        public T GetRandomItem()
+        {
+            var total = 0.0f;
+            var numEntries = _weightedArrayEntries.Count;
+            for(var i = 0; i < numEntries; i++)
+            {
+                total += _weightedArrayEntries[i].Weight;
+            }
+            
+            var currentWeight = Random.Range(0.0f, total);        
+            for (int i = 0; i < numEntries; i++)
+            {
+                currentWeight -= _weightedArrayEntries[i].Weight;
+                if(currentWeight <= 0)
+                {
+                    return _weightedArrayEntries[i].Item;
+                }
+            }
+
+            return default(T);
+        }    
+    }
+}
---
+++ Scripts/WeightedArrayEntry.cs
@@ -0,0 +1,39 @@
+﻿namespace WeightedArrays
+{
+    public class WeightedArrayEntry<T>
+    {
+        private float _weight;
+        private T _item;
+
+        public float Weight
+        {
+            get
+            {
+                return _weight;
+            }
+            set
+            {
+                _weight = value;
+            }
+        }
+
+        public T Item
+        {
+            get
+            {
+                return _item;
+            }
+            set
+            {
+                _item = value;
+            }
+        }
+
+        public WeightedArrayEntry(T item, float weight)
+        {
+            _item = item;
+            _weight = weight;            
+        }
+
+    }   
+}
